ee2d2ec3c6a7622b975c93161ef000d952287359
simranbatth <simrandeepkaur1712@gmail.com>
2026-09-10 20:27:21 +0530

    gemini

--- controllers/dreamController.js
+++ controllers/dreamController.js
@@ -12,7 +12,7 @@ export const addDream = async (req, res) => {
     }
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent`,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
       {
         method: "POST",
         headers: {
